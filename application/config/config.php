c40f83904b0666f78e04123efaebb36cadecad65
Marharrisatama Rizal <marharrisatama@gmail.com>
2020-01-20 14:32:43 +0700

    true

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://10.10.10.94';
+$root = "http://" . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
+//$config['base_url'] = 'https://10.10.10.94';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -35,6 +38,7 @@ $config['base_url'] = 'https://10.10.10.94';
 |
 */
 $config['index_page'] = 'index.php';
+//$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -135,7 +139,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = 'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
