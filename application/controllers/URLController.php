3b1b20e562497ebfb2b4787dbfa2781b6f2a919f
Marharrisatama Rizal <marharrisatama@gmail.com>
2020-01-21 09:55:20 +0700

    true

--- application/controllers/URLController.php
+++ application/controllers/URLController.php
@@ -9,21 +9,32 @@ class URLController extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->datasend = [];
-        if ($this->session->userdata('USERNAME') == NULL) {
-            redirect(site_url("login"));
-            die();
-        }
+//        if ($this->session->userdata('status') <> 'login') {
+//            redirect(site_url("login"));
+//            die();
+//        }
         $this->datasend['SESSION'] = $this->session;
+    }
+
+    public function index() {
+        $this->datasend['formid'] = 'home';
+        $this->datasend['sidebar'] = 'sidebar_view';
+        $this->datasend['content'] = 'home/welcome';
+        $this->load->view('template', $this->datasend);
     }
 
     public function MasterUser() {
         $this->datasend['formid'] = 'home';
+        $this->datasend['sidebar'] = '';
         $this->datasend['content'] = '';
+        $this->load->view('template', $this->datasend);
     }
 
     public function MasterRole() {
         $this->datasend['formid'] = 'home';
+        $this->datasend['sidebar'] = '';
         $this->datasend['content'] = '';
+        $this->load->view('template', $this->datasend);
     }
 
 }
